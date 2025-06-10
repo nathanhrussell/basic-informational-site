8e63da2ae3c3844706b8969b5a801e2396892ffc
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-06-10 16:30:36 +0200

    Install express and rewrite project using that framework

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
 const path = require("path");
 
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  let filePath = "";
+// Serve static files (CSS, JS, images, etc.)
+app.use(express.static("public"));
 
-  switch (req.url) {
-    case "/":
-      filePath = "index.html";
-      break;
-    case "/about":
-      filePath = "about.html";
-      break;
-    case "/contact-me":
-      filePath = "contact-me.html";
-      break;
-    default:
-      filePath = "404.html";
-      res.statusCode = 404;
-  }
-
-  const fullPath = path.join(__dirname, filePath);
-
-  fs.readFile(fullPath, "utf8", (err, data) => {
-    if (err) {
-      res.writeHead(500, { "Content-Type": "text/plain" });
-      res.end("Server error");
-    } else {
-      res.writeHead(res.statusCode || 200, { "Content-Type": "text/html" });
-      res.end(data);
-    }
-  });
+// Route handlers
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-server.listen(port, () => {
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact-me.html"));
+});
+
+// 404 handler - must be after all other routes
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Internal Server Error");
+});
+
+// Start server
+app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
